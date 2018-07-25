c781adbb8a107698cc73627609fd006029f80257
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2018-07-25 15:17:18 +0200

    ..

--- sfinae.cpp
+++ sfinae.cpp
@@ -17,8 +17,39 @@ Foo<T, typename std::enable_if<std::is_copy_constructible<T>::value>::type>::Foo
   /* Phew, we're there */
 }
 
+template<template<typename> typename... Skills>
+class X : public Skills<X<Skills...>>...
+{
+public:
+  void basicMethod() {}
+};
+
+template<typename Derived>
+class ExtraFeature1 {
+ public:
+  void extraMethod1() {
+    auto derived = static_cast<Derived&>(*this);
+    derived.basicMethod();
+    derived.basicMethod();
+    derived.basicMethod();
+  }
+};
+
+template<typename Derived>
+class ExtraFeature2 {
+ public:
+  void extraMethod2() {
+    auto derived = static_cast<Derived&>(*this);
+  }
+};
+
 int main() {
   Foo<float> a;
   // A<std::unique_ptr<float> > b;
+
+  using X12 = X<ExtraFeature1, ExtraFeature2>;
+  X12 x;
+  x.extraMethod1();
+  x.extraMethod2();
   return 0;
 }
