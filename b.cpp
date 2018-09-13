7b4a53f6e09776fdcdd89d848ab1cbfaf5b3bd22
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2018-09-14 00:27:44 +0200

    ..

--- b.cpp
+++ b.cpp
@@ -1,6 +1,7 @@
 #define SPS_DEBUG 1
 
 #include <cstdio>
+#include <cstdint>
 #include <memory>
 
 #include <sps/align.hpp>
@@ -29,8 +30,8 @@ int main() {
 
   std::unique_ptr<A> uA = std::unique_ptr<A>(new A());
   printf("0x%x\n", uintptr_t(uA.get()) % Alignment);
-  std::unique_ptr<A> uA0 = std::make_unique<A>();
-  printf("0x%x\n", uintptr_t(uA0.get()) % Alignment);
+  //  std::unique_ptr<A> uA0 = std::make_unique<A>();
+  //  printf("0x%x\n", uintptr_t(uA0.get()) % Alignment);
 
   return 0;
 }
