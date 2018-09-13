5da60a337ee10f7c99f48c961b86abaccdb48cc0
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2018-09-14 00:07:39 +0200

    ..

---
+++ b.cpp
@@ -0,0 +1,36 @@
+#define SPS_DEBUG 1
+
+#include <cstdio>
+#include <memory>
+
+#include <sps/align.hpp>
+
+const size_t Alignment = 64;
+
+class A : public sps::dynaligned<A, Alignment> {
+ public:
+  A() {
+    printf("A()\n");
+  }
+  ~A() {
+    printf("~A()\n");
+  }
+  char data[4];
+};
+
+
+int main() {
+  A a;
+  printf("0x%x\n", uintptr_t(&a) % Alignment);
+
+  A* p = new A();
+  printf("0x%x\n", uintptr_t(p) % Alignment);
+  delete p;
+
+  std::unique_ptr<A> uA = std::unique_ptr<A>(new A());
+  printf("0x%x\n", uintptr_t(uA.get()) % Alignment);
+  std::unique_ptr<A> uA0 = std::make_unique<A>();
+  printf("0x%x\n", uintptr_t(uA0.get()) % Alignment);
+
+  return 0;
+}
