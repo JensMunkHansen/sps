3fa2970774770cbea8279f1608b9e4ac76f8b5ce
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2018-11-06 20:42:44 +0100

    ..

---
+++ macro_test.cpp
@@ -0,0 +1,45 @@
+#include <gtest/gtest.h>
+
+#include <sps/cenv.h>
+
+template <typename T>
+T tplFuncImpl0(const T& input, int k) {
+  T output = T(0.0);
+  for (int i = 0 ; i < k ; i++) {
+    output += input;
+  }
+  return output;
+}
+
+SPS_ALIAS_TEMPLATE_FUNCTION(tplFunc, tplFuncImpl0);
+
+TEST(macro_test, tpl_alias) {
+  float input = 2.0f;
+  float output = tplFunc(input, 2);
+}
+
+float funcImpl0(const float& input, int k) {
+  float output = 0.0f;
+  for (int i = 0 ; i < k ; i++) {
+    output += input;
+  }
+  return output;
+}
+
+SPS_ALIAS_FUNCTION(func, funcImpl0);
+
+TEST(macro_test, func_alias) {
+  float input = 2.0f;
+  float output = func(input, 2);
+}
+
+int main(int argc, char* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
+/* Local variables: */
+/* indent-tabs-mode: nil */
+/* tab-width: 2 */
+/* c-basic-offset: 2 */
+/* End: */
