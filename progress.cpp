2bbbc5668d958a164f006ad1c8f0933f9e2d0dac
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2018-11-25 21:16:39 +0100

    ..

--- progress.cpp
+++ progress.cpp
@@ -4,35 +4,30 @@
 
 namespace sps {
 
-  void ProgressBar::show(float percent)
-  {
-    // Is this needed???
-    SPS_UNREFERENCED_PARAMETERS(percent);
-  }
+void ProgressBar::show(float percent) {
+  SPS_UNREFERENCED_PARAMETERS(percent);
+}
 
 #if 0
-  void ProgressBar::cancel()
-  {
-    std::unique_lock<std::mutex> lock(mtx);
-    // Order is insignificant so no need for std::atomic<bool>
-    bStop = true;
-    cnd.notify_all();
-  }
+// TODO(JMH): Support stopping
+void ProgressBar::cancel() {
+  std::unique_lock<std::mutex> lock(mtx);
+  // Order is insignificant so no need for std::atomic<bool>
+  bStop = true;
+  cnd.notify_all();
+}
 #endif
 
-  // std::chrono::duration::zero()
-  template <typename P>
-  void ProgressBar::wait(const P& period)
-  {
-    std::unique_lock<std::mutex> lock(mtx);
-    if (bStop || cnd.wait_for(lock, period) == std::cv_status::no_timeout) {
-      bStop = false;
-      //throw 1;//cancelled_error();
-    }
-  }
-
-  void ProgressBarStdOut::show(float percent)
-  {
-    std::cout << "Progress: " << percent << " %" << std::endl;
+// std::chrono::duration::zero()
+template <typename P>
+void ProgressBar::wait(const P& period) {
+  std::unique_lock<std::mutex> lock(mtx);
+  if (bStop || cnd.wait_for(lock, period) == std::cv_status::no_timeout) {
+    bStop = false;
   }
 }
+
+void ProgressBarStdOut::show(float percent) {
+  std::cout << "Progress: " << percent << " %" << std::endl;
+}
+}  // namespace sps
