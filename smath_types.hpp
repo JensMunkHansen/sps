aae724698d35c18bf1f7312c7be78bef4a28f3bc
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2018-12-03 23:06:21 +0100

    poor-mans matvec

--- smath_types.hpp
+++ smath_types.hpp
@@ -131,6 +131,18 @@ struct SPS_EXPORT mat3_t {
     }
     return result;
   }
+
+  point_t<T> operator*(const point_t<T>& vec) {
+    point_t<T> result = {T(0.0), T(0.0), T(0.0)};
+
+    for (size_t i = 0 ; i < 3 ; i++) {
+      for (size_t j = 0 ; j < 3 ; j++) {
+        result[i] = result[i] + data[i][j] * vec[j];
+      }
+    }
+    return result;
+  }
+
   T data[3][3];
 };
 
