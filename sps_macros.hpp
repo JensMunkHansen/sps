1d544c7130873d4887ddc98ce3f68d3a59a3a4f8
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2021-05-22 22:24:13 +0200

    pimpl macros working

--- sps_macros.hpp
+++ sps_macros.hpp
@@ -2,20 +2,28 @@
 
 #include <sps/sps_compiler.h>
 
+template <typename T> inline T *sGetPtrHelper(T *ptr) {
+  return ptr;
+}
+template <typename Ptr> inline auto sGetPtrHelper(Ptr &ptr) -> decltype(ptr.operator->()) {
+  return ptr.operator->();
+}
+
+// The body must be a statement:
 #define SPS_CAST_IGNORE_ALIGN(body) SPS_WARNING_PUSH SPS_WARNING_DISABLE_GCC("-Wcast-align") body SPS_WARNING_POP
 
 #define SPS_DECLARE_PRIVATE(Class) \
     inline Class##Private* d_func() \
-    { SPS_CAST_IGNORE_ALIGN(return reinterpret_cast<Class##Private *>(qGetPtrHelper(d_ptr));) } \
+    { SPS_CAST_IGNORE_ALIGN(return reinterpret_cast<Class##Private *>(sGetPtrHelper(d_ptr));) } \
     inline const Class##Private* d_func() const \
-    { SPS_CAST_IGNORE_ALIGN(return reinterpret_cast<const Class##Private *>(qGetPtrHelper(d_ptr));) } \
+    { SPS_CAST_IGNORE_ALIGN(return reinterpret_cast<const Class##Private *>(sGetPtrHelper(d_ptr));) } \
     friend class Class##Private;
 
 #define SPS_DECLARE_PRIVATE_D(Dptr, Class) \
     inline Class##Private* d_func() \
-    { SPS_CAST_IGNORE_ALIGN(return reinterpret_cast<Class##Private *>(qGetPtrHelper(Dptr));) } \
+    { SPS_CAST_IGNORE_ALIGN(return reinterpret_cast<Class##Private *>(sGetPtrHelper(Dptr));) } \
     inline const Class##Private* d_func() const \
-    { SPS_CAST_IGNORE_ALIGN(return reinterpret_cast<const Class##Private *>(qGetPtrHelper(Dptr));) } \
+    { SPS_CAST_IGNORE_ALIGN(return reinterpret_cast<const Class##Private *>(sGetPtrHelper(Dptr));) } \
     friend class Class##Private;
 
 #define SPS_DECLARE_PUBLIC(Class)                                    \
@@ -26,6 +34,22 @@
 #define SPS_D(Class) Class##Private * const d = d_func()
 #define SPS_Q(Class) Class * const q = q_func()
 
+/*
+  Some classes do not permit copies to be made of an object. These
+  classes contains a private copy constructor and assignment
+  operator to disable copying (the compiler gives an error message).
+*/
+#define SPS_DISABLE_COPY(Class)                 \
+  Class(const Class &) = delete;                \
+  Class &operator=(const Class &) = delete;
+
+#define SPS_DISABLE_MOVE(Class)                 \
+  Class(Class &&) = delete;                     \
+  Class &operator=(Class &&) = delete;
+
+#define SPS_DISABLE_COPY_MOVE(Class)            \
+  Q_DISABLE_COPY(Class)                         \
+  Q_DISABLE_MOVE(Class)
 
 
 
