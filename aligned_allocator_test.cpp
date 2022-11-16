2365db1b17ea7e867e66a0fb8c30304a5358c76a
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2022-11-16 20:22:19 +0000

    stack alloc

--- aligned_allocator_test.cpp
+++ aligned_allocator_test.cpp
@@ -17,17 +17,18 @@ TEST(aligned_allocator, unique_array_test) {
 
 TEST(aligned_allocator, unique_aligned_array_test) {
   auto z =
-      sps::make_unique_aligned_array<Foo, 16>(2, 3, 4);
+    sps::make_unique_aligned_array<Foo, 16>(2, 3, 4);
   ASSERT_TRUE((uintptr_t) &(z.get()[1]) % 16 == 0);
 }
 
 TEST(aligned_allocator, unique_aligned_array_test_max_align) {
   auto z =
-      sps::make_unique_aligned_array<Foo, 32>(2, 3, 4);
+    sps::make_unique_aligned_array<Foo, 32>(2, 3, 4);
   ASSERT_TRUE((uintptr_t) &(z.get()[1]) % 32 == 0);
 }
 
 int main(int argc, char* argv[]) {
+  float* p = (float*) alloca(4*sizeof(float));
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
