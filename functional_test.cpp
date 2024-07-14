42eaf632f4e39a4eba1ca678b051b9779bef1157
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-07-14 18:48:53 +0200

    func test

--- functional_test.cpp
+++ functional_test.cpp
@@ -19,6 +19,7 @@ int i(int a, int b, int c, int d) {
   return 1 + 2*a + b + c + d;
 }
 
+
 TEST(functional_test, prebinder) {
   using sps::trailing_bind;
   using sps::prebind;
@@ -48,6 +49,19 @@ TEST(functional_test, prebinder) {
   //  and the one you wanted orginally
   auto e = prebind(i, 2, 1, 1);
   ASSERT_EQ(e(1), 8);
+}
+
+// Stack-allocated
+void test(void* ptr) {
+  size_t a[40]{};
+  a[0]= sizeof(a);
+  std::cout << (reinterpret_cast<char const *>(a) - reinterpret_cast<char *>(ptr)) << " " << a[0] << "\n";
+}
+
+
+TEST(abe, test) {
+  sps::static_function<void(void*)> h(test);
+  h(&h);
 }
 
 int main(int argc, char* argv[]) {
